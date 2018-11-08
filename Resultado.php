9006c54515ef03f1f04c80812b2e507b0379f2a9
vakinhas <44876470+vakinhas@users.noreply.github.com>
2018-11-08 17:47:19 -0200

    Add files via upload

---
+++ Resultado.php
@@ -0,0 +1,13 @@
+<?php
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$sexo = $_POST['sexo'];
+$regiao = $_POST['regiao'];
+$opiniao = $_POST['opiniao'];
+
+print "Nome: <strong> $nome </strong> </br>";
+print "E-mail: <strong> $email </strong> </br>";
+print "Sexo: <strong> $sexo </strong> </br>";
+print "Opinião: <strong> $opiniao </strong> </br>";
+
+?>
